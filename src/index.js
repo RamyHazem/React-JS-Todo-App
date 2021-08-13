bb7b3b48f8c332f956e734845c4ed63ca60c0681
Ramy Awad <ramyaqt20@gmail.com>
2021-08-13 12:03:37 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const [selected, setSelected] = useState([]);
+
+//   const toggleTopping = ({ target }) => {
+//     const clickedTopping = target.value;
+//     setSelected((prev) => {
+//       // check if clicked topping is already selected
+//       if (prev.includes(clickedTopping)) {
+//         // filter the clicked topping out of state
+//         return prev.filter((t) => t !== clickedTopping);
+//       } else {
+//         // add the clicked topping to our state
+//         return [clickedTopping, ...prev];
+//       }
+//     });
+//   };
+
+//   return (
+//     <div>
+//       {options.map((option) => (
+//         <button value={option} onClick={toggleTopping} key={option}>
+//           {selected.includes(option) ? "Remove " : "Add "}
+//           {option}
+//         </button>
+//       ))}
+//       <p>Order a {selected.join(", ")} pizza</p>
+//     </div>
+//   );
+// }
